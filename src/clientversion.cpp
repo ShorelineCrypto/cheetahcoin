32e8501e5732668370b52ac634d34dd7e9f1d182
ShorelineCrypto <admin@shorelinecrypto.com>
2022-03-06 02:43:54 -0500

    update version to v2.1.0

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -18,7 +18,7 @@ const std::string CLIENT_NAME("Cheetah");
 /**
  * Client version number
  */
-#define CLIENT_VERSION_SUFFIX ""
+#define CLIENT_VERSION_SUFFIX "-1.10.x"
 
 
 /**
@@ -50,13 +50,13 @@ const std::string CLIENT_NAME("Cheetah");
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-" DO_STRINGIZE(suffix)
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-randomSpike"
 
 #define BUILD_DESC_FROM_COMMIT(maj, min, rev, build, commit) \
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-g" commit
 
 #define BUILD_DESC_FROM_UNKNOWN(maj, min, rev, build) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-unk"
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-randomSpike"
 
 #ifndef BUILD_DESC
 #ifdef BUILD_SUFFIX
