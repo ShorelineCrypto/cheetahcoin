7cf0156a1bef13736ba62b70e3b474de2a6e7077
ShorelineCrypto <admin@shorelinecrypto.com>
2022-03-08 22:34:32 -0500

    update scripts readme

--- doc/Android_Userland_App/armhf/prepare_chta.sh
+++ doc/Android_Userland_App/armhf/prepare_chta.sh
@@ -11,8 +11,13 @@ sudo apt -y install python2
 
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
 sudo  python2 get-pip.py
+sudo pip2 install python-bitcoinrpc
 
-git clone https://github.com/ShorelineCrypto/cheetah_cpuminer.git
+git clone https://github.com/ShorelineCrypto/cheetah_cpuminer.git cheetah_cpuminer_chta
+cd cheetah_cpuminer_chta
+git checkout chta
+
+cd ..
 
 wget  https://github.com/ShorelineCrypto/cheetahcoin/releases/download/v2.1.0/cheetahcoin_2.1.0_armhf_linux-gnu.tgz
 tar xvfz cheetahcoin_2.1.0_armhf_linux-gnu.tgz 
