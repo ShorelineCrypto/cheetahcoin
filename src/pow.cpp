e6b119362f1067cd9068cfe8fc3347e1ecf3ee9a
ShorelineCrypto <admin@shorelinecrypto.com>
2026-08-15 12:15:17 -0400

    remove lock after spike block mined, allow regular diff

--- src/pow.cpp
+++ src/pow.cpp
@@ -47,9 +47,9 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
                 return nCheetah;
             else if  ((pblock->nTime > pindexLast->nTime + 80) || (pblock->nTime < pindexLast->nTime - 80))
             {
-                // Return the last non-special-min-difficulty-rules-block
+                // Return the last non-special-nonSpike-nonCheetah-block
 
-                while (pindex->pprev && pindex->nHeight % nInterval != 0 && pindex->nBits == nCheetah)
+                while (pindex->pprev && pindex->nHeight % nInterval != 0 && pindex->nBits == nCheetah && pindex->nBits == nSpike)
                     pindex = pindex->pprev;
                 return pindex->nBits;
             }
@@ -64,7 +64,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
                     return nSpike;
                 else
                 {
-                    while (pindex->pprev && pindex->nHeight % nInterval != 0 && pindex->nBits == nCheetah)
+                    while (pindex->pprev && pindex->nHeight % nInterval != 0 && pindex->nBits == nCheetah && pindex->nBits == nSpike)
                         pindex = pindex->pprev;
                     return pindex->nBits;
                 }
@@ -80,7 +80,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
                     return nSpike;
                 else
                 {
-                    while (pindex->pprev && pindex->nHeight % nInterval != 0 && pindex->nBits == nCheetah)
+                    while (pindex->pprev && pindex->nHeight % nInterval != 0 && pindex->nBits == nCheetah && pindex->nBits == nSpike)
                         pindex = pindex->pprev;
                     return pindex->nBits;
                 }
@@ -96,7 +96,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
                     return nSpike;
                 else
                 {
-                    while (pindex->pprev && pindex->nHeight % nInterval != 0 && pindex->nBits == nCheetah)
+                    while (pindex->pprev && pindex->nHeight % nInterval != 0 && pindex->nBits == nCheetah && pindex->nBits == nSpike)
                         pindex = pindex->pprev;
                     return pindex->nBits;
                 }
@@ -112,7 +112,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
                     return nSpike;
                 else
                 {
-                    while (pindex->pprev && pindex->nHeight % nInterval != 0 && pindex->nBits == nCheetah)
+                    while (pindex->pprev && pindex->nHeight % nInterval != 0 && pindex->nBits == nCheetah && pindex->nBits == nSpike)
                         pindex = pindex->pprev;
                     return pindex->nBits;
                 }
